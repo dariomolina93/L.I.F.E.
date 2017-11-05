cf7395187d46d380adbac938d86ba1058381e67a
Dario Molina <dariomolina93>
2017-11-05 00:26:47 -0700

    Modified the app logo and created a new class for enroll.

---
+++ app/src/main/java/com/example/dariomolina/life/Enroll.java
@@ -0,0 +1,15 @@
+package com.example.dariomolina.life;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dariomolina on 11/4/17.
+ */
+
+public class Enroll extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/example/dariomolina/life/MainActivity.java
+++ app/src/main/java/com/example/dariomolina/life/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.dariomolina.life;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button enroll;
+    private Button steam;
+    private Button supporters;
+    private Button involved;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enroll = (Button) findViewById(R.id.enroll);
+
+        enroll.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent enroll = new Intent(getApplicationContext(), Enroll.class);
+                startActivity(enroll);
+                return;
+            }
+        });
+        steam = (Button) findViewById(R.id.steam);
+        supporters = (Button) findViewById(R.id.supporters);
+        involved = (Button) findViewById(R.id.involved);
     }
 }
